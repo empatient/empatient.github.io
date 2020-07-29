10ce2eb807018552fbbc98e1a4cc737a5e812e9c
Julian Reed <37030111+julian-reed@users.noreply.github.com>
2020-07-29 10:20:36 -0400

    Added Julian's files from first attempt with extension

---
+++ new.js
@@ -0,0 +1,68 @@
+
+<script src="https://download.affectiva.com/js/3.2.1/affdex.js"/>
+
+/*
+   Face detector configuration - If not specified, defaults to F
+   affdex.FaceDetectorMode.LARGE_FACES
+   affdex.FaceDetectorMode.LARGE_FACES=Faces occupying large portions of the frame
+   affdex.FaceDetectorMode.SMALL_FACES=Faces occupying small portions of the frame
+*/
+var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+
+//Construct a FrameDetector and specify the image width / height and face detector mode.
+var detector = new affdex.FrameDetector(faceMode);
+
+detector.detectAllExpressions();
+detector.detectAllEmotions();
+detector.detectAllEmojis();
+detector.detectAllAppearance();
+
+detector.addEventListener("onInitializeSuccess", function() {});
+detector.addEventListener("onInitializeFailure", function() {});
+
+/* 
+  onImageResults success is called when a frame is processed successfully and receives 3 parameters:
+  - Faces: Dictionary of faces in the frame keyed by the face id.
+           For each face id, the values of detected emotions, expressions, appearane metrics 
+           and coordinates of the feature points
+  - image: An imageData object containing the pixel values for the processed frame.
+  - timestamp: The timestamp of the captured image in seconds.
+*/
+detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {});
+
+/* 
+  onImageResults success receives 3 parameters:
+  - image: An imageData object containing the pixel values for the processed frame.
+  - timestamp: An imageData object contain the pixel values for the processed frame.
+  - err_detail: A string contains the encountered exception.
+*/
+detector.addEventListener("onImageResultsFailure", function (image, timestamp, err_detail) {});
+
+detector.addEventListener("onResetSuccess", function() {});
+detector.addEventListener("onResetFailure", function() {});
+
+detector.addEventListener("onStopSuccess", function() {});
+detector.addEventListener("onStopFailure", function() {});
+
+detector.start();
+
+//Get a canvas element from DOM
+var aCanvas = document.getElementById("canvas");
+var context = aCanvas.getContext('2d');
+
+//Cache the timestamp of the first frame processed
+var startTimestamp = (new Date()).getTime() / 1000;
+
+//Get imageData object.
+var imageData = context.getImageData(0, 0, 640, 480);
+
+//Get current time in seconds
+var now = (new Date()).getTime() / 1000;
+
+//Get delta time between the first frame and the current frame.
+var deltaTime = now - startTimestamp;
+
+//Process the frame
+detector.process(imageData, deltaTime);
+
+detector.stop();
