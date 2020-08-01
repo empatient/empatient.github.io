b9a395a4ae0cebada096021c9498e7a28ab346d4
Julian <juliantreed@gmail.com>
2020-08-01 18:52:09 -0400

    Added functionality to load video file into extension

--- analysis.js
+++ analysis.js
@@ -1,3 +1,38 @@
 //Step 1: Load in file using video tag in HTML
 //Step 2: Draw to the canvas until the image data can be drawn using getImageData()
 //Step 3: Process this data and analyze it
+
+//Loading in the file:
+
+//HTML code required:
+//<input type="file" accept="video/*"/>
+//<video controls autoplay></video>
+
+(function localFileVideoPlayer() {
+	'use strict'
+  var URL = window.URL || window.webkitURL
+  var displayMessage = function (message, isError) {
+    var element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+})()
